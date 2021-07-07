7e29cae5aed438e50438c95dd031d07d94fd2cd1
Chelsea Rio <chelseario@gmail.com>
2021-07-07 15:20:05 -0400

    CIRC-4594 Added page sidenav. Still lots of work to do here to highlight and expand items appropriately. Also need to hide this nav on mobile.

--- static/js/headings.js
+++ static/js/headings.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     $("#content > h2, #content > h3, #content > h4, #content > h5, #content > h6").each(function( index ) {
+        var $headingTitle = $(this).text();
         var $headingID = $(this).attr("id");
         var $headingLink = $("<a class='heading-link' href='#" + $headingID + "'><i class='far fa-link'></i></a>");
+        var $sidenavLink = $("<li><a class='sidenav-link-level-" + $(this)[0].tagName + "' href='#" + $headingID + "'>" + $headingTitle + "</a></li>");
         $(this).append($headingLink);
+        $('.cd-sidenav-nav').append($sidenavLink);
     });
+
+
     
     $('.heading-link').click(function(){
         $('html,body').animate({scrollTop: ($(this).offset().top - 70)}, 'slow');
